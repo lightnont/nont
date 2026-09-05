e699130c127b54e0dbfde04b5e9c9186addd52f4
light <lightnont@gmail.com>
2026-09-05 19:55:53 +0200

    Delete build-exe.bat

--- build-exe.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-set "RUSTUP_HOME=%USERPROFILE%\.rustup"
-set "CARGO_HOME=%USERPROFILE%\.cargo"
-call npx.cmd tauri build
-endlocal
